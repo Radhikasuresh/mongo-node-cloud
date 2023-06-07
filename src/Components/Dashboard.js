0589fd73a2bcb387c7cd50228e4f56c4bb6540d8
Radhikasuresh <radhikasuresh550@gmail.com>
2023-06-07 20:13:07 +0530

    first commit

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Base from '../Base/Base'
+import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
+    const navigate = useNavigate()
   return (
     <Base
     title={"Welcome to Students application"}
     description={"This is a student data application"}
     >
      <h1>Please navigate to students app</h1>
+     <button 
+     className='buttonn'
+     onClick={()=>navigate("/login")}>Login to the Website</button>
     </Base>
   )
 }
